4931025869b777758d94acddeb116a568c50ecb9
cristophersolis003 <cristophersolis003@gmail.com>
2024-07-25 19:45:39 +0000

    add SEO[A

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://4321-idx-astro-avanzado-1720108738756.cluster-m7tpz3bmgjgoqrktlvd4ykrc2m.cloudworkstations.dev/",
+  integrations: [tailwind(), icon({
+    include: {
+      lucide: ['*']
+    }
+  }), sitemap()]
 });
